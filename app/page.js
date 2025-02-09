b2413b2f1b4b127f2afb87a3b888d7ad0952afe8
Aman Singh <amansingh080704@gmail.com>
2025-02-09 19:33:10 +0530

    initial changes

---
+++ app/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import HeroSection from "../components/HeroSection";
+
+export default function Home() {
+  return (
+   
+
+<div className="container mx-auto p-8 pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)] ">
+<HeroSection />
+    </div>
+  );
+}
